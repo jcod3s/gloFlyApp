28a45237cbafa4df5a678049fdd9c31e88009e19
jcod3s <98285717+jcod3s@users.noreply.github.com>
2023-01-07 13:33:06 -0500

    backend API call

--- controllers/search.js
+++ controllers/search.js
@@ -1,18 +1,76 @@
 // import { useRef, useEffect, useState } from 'react'
-// import * as tt from '@tomtom-international/web-sdk-maps'
+import * as tt from '@tomtom-international/web-sdk-maps'
 const Post = require("../models/Post")
 
 module.exports = {
 
   getSearch: async (req, res) => {
-      try {
-        const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-        res.render('search.ejs', { posts: posts})
-      } catch (err) {
-        console.log(err);
+      // additions
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+      
+      function success(pos) {
+        const crd = pos.coords;
+        const lat = crd.latitude;
+        const long = crd.longitude;
+    
+        let center = [long,lat]
+        const map = tt.map({
+            key: process.env.TOM_TOM_API_KEY,
+            container: "map",
+            center: center,
+            zoom: 10
+        })
+        map.on('load', () => {
+            new tt.Marker().setLngLat(center).addTo(map)
+        })
       }
+      
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+      // end additions
+        
+      try {
+          const posts = await Post.find().sort({ createdAt: "desc" }).lean();
+          res.json(options, success, error)
+          res.render('search.ejs', { posts: posts})
+        } catch (err) {
+          console.log(err);
+        }
     },
   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     getLocation: async (req, res) => {
     try {
       const options = {
@@ -47,6 +105,7 @@ module.exports = {
       //
     }
   },
+
   getMap: async (req, res) => {
     try {
       //
@@ -54,6 +113,7 @@ module.exports = {
       //
     }
   },
+
   getExperiences: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
--- public/js/search.js
+++ public/js/search.js
@@ -1,29 +1,29 @@
 //define get user location function
-const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
+// const options = {
+//     enableHighAccuracy: true,
+//     timeout: 5000,
+//     maximumAge: 0
+//   };
   
-  function success(pos) {
-    const crd = pos.coords;
-    const lat = crd.latitude;
-    const long = crd.longitude;
+//   function success(pos) {
+//     const crd = pos.coords;
+//     const lat = crd.latitude;
+//     const long = crd.longitude;
 
-    let center = [long,lat]
-    const map = tt.map({
-        key: 't0p2tntCvwAXFp21RwqwKhUkUpgFVpbO',
-        container: "map",
-        center: center,
-        zoom: 10
-    })
-    map.on('load', () => {
-        new tt.Marker().setLngLat(center).addTo(map)
-    })
-  }
+//     let center = [long,lat]
+//     const map = tt.map({
+//         key: 't0p2tntCvwAXFp21RwqwKhUkUpgFVpbO',
+//         container: "map",
+//         center: center,
+//         zoom: 10
+//     })
+//     map.on('load', () => {
+//         new tt.Marker().setLngLat(center).addTo(map)
+//     })
+//   }
   
-  function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
+//   function error(err) {
+//     console.warn(`ERROR(${err.code}): ${err.message}`);
+//   }
 
 navigator.geolocation.getCurrentPosition(success, error, options)
